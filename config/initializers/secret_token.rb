7c9bf380dbbdc44560c86a0271711dd9c44dfb83
Chris Adams <chrisadams@Chris-Adamss-MacBook-Pro.local>
2011-09-21 12:31:56 +0100

    Initial commit without loads of redundant crap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cico2::Application.config.secret_token = '6b24d2ca161a7b6a02f94637ad257e29519e1cfaf1b942e01ed9f9108b370211f433620043c93e01172b55d245e008e471d724e6f993dc67a021cdcf4f03fd8b'
