a6e2c7dd37f039b8f8db7d29bcf8ca746fd541b9
Chris Adams <wave@chrisadams.me.uk>
2011-09-25 09:54:52 +0100

    Add distance calculator method using haversine

--- spec/helpers/footprint_helper_spec.rb
+++ spec/helpers/footprint_helper_spec.rb
@@ -3,13 +3,11 @@ require 'spec_helper'
 # Specs in this file have access to a helper object that includes
 # the FootprintHelper. For example:
 #
-# describe FootprintHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
-describe FootprintsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe FootprintsHelper do  
+    
+  it "gives the distance between to latlngs, using the haversine formula" do
+    off_broadway_bar_london = [51.514667, -0.136047]
+    the_marksman_pub = [51.502557, -0.07253941]
+    helper.distance_between_points(off_broadway_bar_london, the_marksman_pub).to_meters.should be_within(0.1).of(4596.84510626154)
+  end
 end
