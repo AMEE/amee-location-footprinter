4940743c1f85a44768806e40074ffbdb13a0bbc2
Chris Adams <wave@chrisadams.me.uk>
2011-09-25 22:26:20 +0100

    Tidy up distance calculation method to use Models instead of Foursquare objects

--- app/helpers/footprints_helper.rb
+++ app/helpers/footprints_helper.rb
@@ -3,28 +3,8 @@ module FootprintsHelper
   # TODO make distance formatting methods here to avoid excessively long figures
 
   def distance_between_points(checkin1, checkin2)
-    # TODO make this check for the foursquare object, instead of just
-    # not being an array
-    if checkin1.class != Array
-      # https://github.com/kristianmandrup/haversine/blob/master/lib/haversine.rb
-      # self.distance(lat1, lon1, lat2=nil, lon2=nil)
-      lon1 = checkin1.venue.location.lng
-      lat1 = checkin1.venue.location.lat
-    else
-      lat1 = checkin1.first
-      lon1 = checkin1.last
-    end
 
-    if checkin2.class != Array
-      binding.pry
-      lon2 = checkin2.venue.location.lng
-      lat2 = checkin2.venue.location.lat
-    else
-      lat2 = checkin2.first
-      lon2 = checkin2.last
-    end
-  
-    distance = Haversine.distance(lat1, lon1, lat2, lon2)
+    distance = Haversine.distance(checkin1.lat.to_f, checkin1.lon.to_f, checkin2.lat.to_f, checkin2.lon.to_f)
     distance
   end
 
--- spec/helpers/footprint_helper_spec.rb
+++ spec/helpers/footprint_helper_spec.rb
@@ -5,9 +5,10 @@ require 'spec_helper'
 #
 describe FootprintsHelper do  
     
-  it "gives the distance between to latlngs, using the haversine formula" do
-    off_broadway_bar_london = [51.514667, -0.136047]
-    the_marksman_pub = [51.502557, -0.07253941]
-    helper.distance_between_points(off_broadway_bar_london, the_marksman_pub).to_meters.should be_within(0.1).of(4596.84510626154)
+  it "should give the distance between two latlngs, using the haversine formula" do
+    checkin1 = FactoryGirl.build(:checkin, :lat => "51.514667", :lon => "-0.136047") # Off Broadway Bar, London
+    checkin2 = FactoryGirl.build(:checkin, :lat => "51.502557", :lon => "-0.07253941") # The Marksman Pub, London
+    helper.distance_between_points(checkin1, checkin2).to_meters.should be_within(0.1).of(4596.84510626154)
   end
+  
 end
