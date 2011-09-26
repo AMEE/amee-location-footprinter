4b9e59bd71c40207d7548982d8af2199eb880b63
Chris Adams <wave@chrisadams.me.uk>
2011-09-26 10:56:10 +0200

    Make sure spec code is in correct 'it' block

--- spec/helpers/footprint_helper_spec.rb
+++ spec/helpers/footprint_helper_spec.rb
@@ -13,6 +13,12 @@ describe FootprintsHelper do
   
   use_vcr_cassette "fetching_carbon_for_walking"
   it "should calculate distances up to 1km based on CO2 from walking" do
+    pending
+  end
+
+  use_vcr_cassette "fetching_carbon_for_driving"
+  it "should calculate distances of between 1km and 200km based on CO2 from driving" do
+
     # create two checkins using the factories
     checkin1 = FactoryGirl.build(:checkin, :lat => "51.514667", :lon => "-0.136047") # Off Broadway Bar, London
     checkin2 = FactoryGirl.build(:checkin, :lat => "51.502557", :lon => "-0.07253941") # The Marksman Pub, London  
@@ -20,18 +26,13 @@ describe FootprintsHelper do
     # fetch the distance
     distance = helper.distance_between_points(checkin1, checkin2)
     
-    distance.to_meters.should be_within(0.1).of(4596.84510626154)
+    distance.to_kilometers.should be_within(0.1).of(4.59684510626154)
 
     # fetch the carbon 
     helper.co2_for_km(distance.to_meters)
 
-    helper.should_receive(:carbon_for_car).with(distance.to_meters).and_return(0.940468540290048)
+    helper.should_receive(:carbon_for_car).with(distance.to_kilometers).and_return(0.940468540290048)
 
-  end
-
-  use_vcr_cassette "fetching_carbon_for_driving"
-  it "should calculate distances of between 1km and 200km based on CO2 from driving" do
-    pending
   end
 
   use_vcr_cassette "fetching_carbon_for_domestic_flights"    
