aa0749380803b6fa5e70f3d1b79f7fb025bf0db5
Chris Adams <wave@chrisadams.me.uk>
2011-09-27 16:45:47 +0100

    Move helper into the model instead

--- app/helpers/footprints_helper.rb
+++ app/helpers/footprints_helper.rb
@@ -12,43 +12,5 @@ module FootprintsHelper
   # to the AMEE abstraction layer.
   # 
   # @param [Float] Distance of the journey we're trying to measure for
-  def co2_for_km(distance)
-    case distance
-    when 0..1
-      # AMEE always returns '0' for walking, so we can return 
-      carbon_for('walking', distance)
-    when 1..200
-      carbon_for('car', distance)
-    when 200..1000
-      carbon_for('domestic_flight', distance)
-    when 1000..3000
-      carbon_for('short_haul_flight', distance)
-    when distance > 3000
-      carbon_for('long_haul_flight', distance)
-    else
-      raise Exception
-    end
-
-  end
-
-  def carbon_for(transport, distance)
-    puts "#{transport}, #{distance}"
-    return 0 if transport == 'walking'
-    calculate_co2e_for_distance(distance, Calculations[transport.to_sym])
-  end
-
-
-
-  def calculate_co2e_for_distance(distance, calculation_prototype)
-
-    c = calculation_prototype.begin_calculation
-
-    c.choose( 
-    :amount => distance,
-    :name => rand(10e6)
-    )
-    c.calculate!
-    # add calculate and save here
-    c[:co2e].value
-  end
+  
 end
--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -2,5 +2,49 @@ class Checkin < ActiveRecord::Base
     belongs_to :user
   
     validates :lat, :lon, :foursquare_id, :timestamp, :venue_name, :presence => true
+
+    def self.distance_between_points(checkin1, checkin2)
+
+      distance = Haversine.distance(checkin1.lat.to_f, checkin1.lon.to_f, checkin2.lat.to_f, checkin2.lon.to_f)
+      distance
+    end
+   
+    def self.co2_for_km(distance)
+      case distance
+      when 0..1
+        # AMEE always returns '0' for walking, so we can return 
+        Checkin.carbon_for('walking', distance)
+      when 1..200
+        Checkin.carbon_for('car', distance)
+      when 200..1000
+        Checkin.carbon_for('domestic_flight', distance)
+      when 1000..3000
+        Checkin.carbon_for('short_haul_flight', distance)
+      when distance > 3000
+        Checkin.carbon_for('long_haul_flight', distance)
+      else
+        raise Exception
+      end
+
+    end
+
+    def self.carbon_for(transport, distance)
+      puts "#{transport}, #{distance}"
+      return 0 if transport == 'walking'
+      Checkin.calculate_co2e_for_distance(distance, Calculations[transport.to_sym])
+    end
+
+    def self.calculate_co2e_for_distance(distance, calculation_prototype)
+
+      c = calculation_prototype.begin_calculation
+
+      c.choose( 
+      :amount => distance,
+      :name => rand(10e6)
+      )
+      c.calculate!
+      # add calculate and save here
+      c[:co2e].value
+    end
      
 end
