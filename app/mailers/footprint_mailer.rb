d9c68d29031d2b96a7b08a2b83775adbf39f4449
Chris Adams <wave@chrisadams.me.uk>
2011-09-27 17:38:19 +0100

    Add formatting of email

--- app/mailers/footprint_mailer.rb
+++ app/mailers/footprint_mailer.rb
@@ -3,8 +3,9 @@ class FootprintMailer < ActionMailer::Base
 
   def footprint_email(user)
     @user = user
+    @total_co2 = 0
     @url  = "http://example.com/login"
-    mail(:to => user.email, :subject => "Welcome to My Awesome Site")
+    mail(:to => user.email, :subject => "Check out your Checkins")
   end
 
 end
