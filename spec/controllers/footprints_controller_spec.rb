5d168939beafdfc64d13b8483ea0df0f2a6e7c76
Chris Adams <wave@chrisadams.me.uk>
2011-09-28 13:34:49 +0100

    Minor update to specs

--- spec/controllers/footprints_controller_spec.rb
+++ spec/controllers/footprints_controller_spec.rb
@@ -7,8 +7,24 @@ describe FootprintsController do
     it "should fetch content from foursquare and amee" do
       # this is a working API token for Chris Adams - YMMV
       session["access_token"] = "GLW4QILQIABBXXTVOIR4FG25YXXPXVEMCF4V2A22GFBGUD2P"
+
+
+      # mock out calls to quimby
+      flexmock(Foursquare::Base).new_instances do |foursquare|
+        foursquare.should_receive(:find).with(:self)
+      end
+
+
+      # mock out calls to AMEE abstraction layer
+      flexmock(AMEE::DataAbstraction::OngoingCalculation).new_instances do |mock|
+        mock.should_receive(:choose).and_return(nil)
+        mock.should_receive(:calculate!).and_return(nil)
+        mock.should_receive(:[]).with(:co2e).and_return(flexmock(:value => 42))
+      end
+
       get 'user'
       response.should be_success
+
     end
     
     # <% 
--- spec/mailers/footprint_mailer_spec.rb
+++ spec/mailers/footprint_mailer_spec.rb
@@ -1,5 +1,30 @@
 require "spec_helper"
 
 describe FootprintMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    describe 'carbon footprint mail' do
+      # pass the user model into the Mailer
+
+      # make our user
+      let(:user) { FactoryGirl.build(:user) }
+      let(:mail) { FootprintMailer.footprint_email(user) }
+
+      # #ensure that the subject is correct
+      it 'renders the subject' do
+        # pending
+        mail.subject.should == 'Check out your Checkins'
+      end
+
+      # #ensure that the @name variable appears in the email body
+      # it 'assigns @name' do
+      #   mail.body.encoded.should match(user.name)
+      # end
+      #
+      # #ensure that the @confirmation_url variable appears in the email body
+      # it 'assigns @confirmation_url' do
+      #   mail.body.encoded.should match("http://aplication_url/#{user.id}/confirmation")
+      # end
+
+  end
+
 end
