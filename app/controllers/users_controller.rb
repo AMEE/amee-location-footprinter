cf49fe9438c7b4e4a56b4a99606389499ca9a5b7
Chris Adams <wave@chrisadams.me.uk>
2011-09-29 15:23:51 +0100

    Stop hardcoding redirect_url in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
   def new
+    redirect_url = Rails.configuration.foursquare_redirect_url
     return redirect_to footprints_user_path if current_user
-    @authorize_url = foursquare.authorize_url("http://localhost:5000/user/callback")
+    @authorize_url = foursquare.authorize_url(redirect_url)
 
   end
 
   def callback
+    redirect_url = Rails.configuration.foursquare_redirect_url
     code = params[:code]
-    @access_token = foursquare.access_token(code, "http://localhost:5000/user/callback")
+    @access_token = foursquare.access_token(code, redirect_url)
     session[:access_token] = @access_token
     
     redirect_to footprints_user_path
