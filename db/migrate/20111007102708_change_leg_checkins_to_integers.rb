d09544e93a4142570c221fa22354c38d5cd564bc
Chris Adams <wave@chrisadams.me.uk>
2011-10-10 15:39:07 +0100

    Make sure foreign keys are ints, not strings

--- db/migrate/20111007102708_change_leg_checkins_to_integers.rb
+++ db/migrate/20111007102708_change_leg_checkins_to_integers.rb
@@ -1,14 +1,16 @@
 class ChangeLegCheckinsToIntegers < ActiveRecord::Migration
   def self.up
     remove_column :legs, :start_checkin
+    remove_column :legs, :end_checkin
     add_column :legs, :start_checkin, :int
-    
+    add_column :legs, :end_checkin, :int    
   end                  
                        
   def self.down        
     remove_column :legs, :start_checkin
+    remove_column :legs, :end_checkin
     add_column :legs, :start_checkin, :string
-
+    add_column :legs, :end_checkin, :string
 
   end
 end
