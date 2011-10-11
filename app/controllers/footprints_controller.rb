b0774d9681515b46de3088eccf72ecc0e5dbbae6
Chris Adams <wave@chrisadams.me.uk>
2011-10-11 12:54:13 +0100

    Move leg and checkin creation into Model

--- app/controllers/footprints_controller.rb
+++ app/controllers/footprints_controller.rb
@@ -17,7 +17,7 @@ class FootprintsController < ApplicationController
     end
 
     # let delayed job taker care of the processing
-    calculate_carbon_and_send_mail( u, current_user.checkins)
+    Checkin.calculate_carbon_and_send_mail(u, current_user.checkins, request)
 
     redirect_to footprints_thanks_url
 
@@ -30,62 +30,5 @@ class FootprintsController < ApplicationController
   def checkins
   end
 
-
-  private
-
-    def calculate_carbon_and_send_mail(u, checkins)
-
-      # cache them in the database
-      checkins.each_with_index do |checkin, index|
-        c = u.checkins.find_or_create_by_foursquare_id(checkin.id)
-
-        if c.new_record?
-
-          c.update_attributes!({
-           :lat => checkin.json['type'] == 'venueless' ? checkin.json['location']['lat'] : checkin.venue.location.lat,
-           :lon => checkin.json['type'] == 'venueless' ? checkin.json['location']['lng'] : checkin.venue.location.lng,
-           :timestamp => checkin.created_at,
-           :timezone => checkin.timezone,
-           :venue_name => checkin.json['type'] == 'venueless' ? checkin.json['location']['name'] : checkin.venue.name,
-           :foursquare_id => checkin.id,
-          })
-          c.save!
-
-          # we need to be at least in the second iteration of the loop
-          # fetch our prev_checkin object
-          if index > 0
-            # make a compound index
-            prev_checkin = Checkin.find_by_foursquare_id(current_user.checkins[index-1].id)
-            current_checkin = Checkin.find_by_foursquare_id(current_user.checkins[index].id)
-
-            # surely there's a nicer way to  write this?
-            l = u.legs.find_or_initialize_by_start_checkin_id_and_end_checkin_id(prev_checkin.id, current_checkin.id)
-
-            if l.new_record?
-              distance = Checkin.distance_between_points(current_checkin, prev_checkin).to_km
-
-              l.update_attributes!({
-               :distance => distance,
-               :co2 => Checkin.co2_for_km(distance),
-               :timestamp => current_checkin.timestamp,
-               :timezone => current_checkin.timezone,
-              })
-
-              l.save!
-            end
-
-          end
-        end
-      end
-      # TODO turn to named scope - return list of checkins from the last 7 days
-      @legs = u.legs.where("timestamp > ?", Date.current-7.day )
-
-      # using delay makes this act as a delayed job
-      # http://rdoc.info/github/collectiveidea/delayed_job/master/file/README.textile#Gory_Details
-      FootprintMailer.footprint_email(u, @legs, request.host_with_port).deliver!
-
-    end
-
-    handle_asynchronously :calculate_carbon_and_send_mail
 
 end
--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -50,6 +50,68 @@ class Checkin < ActiveRecord::Base
       c[:co2e].value
     end
 
+    def self.calculate_carbon_and_send_mail(user, checkins, request)
+
+      # cache them in the database
+      checkins.each_with_index do |checkin, index|
+        c = user.checkins.find_or_create_by_foursquare_id(checkin.id)
+
+        if c.new_record?
+
+          c.update_attributes!({
+           :lat => checkin.json['type'] == 'venueless' ? checkin.json['location']['lat'] : checkin.venue.location.lat,
+           :lon => checkin.json['type'] == 'venueless' ? checkin.json['location']['lng'] : checkin.venue.location.lng,
+           :timestamp => checkin.created_at,
+           :timezone => checkin.timezone,
+           :venue_name => checkin.json['type'] == 'venueless' ? checkin.json['location']['name'] : checkin.venue.name,
+           :foursquare_id => checkin.id,
+           :icon => checkin.json['type'] == 'venueless' ? "https://foursquare.com/img/categories/none.png" : checkin.venue.icon
+          })
+          c.save!
+
+          # we need to be at least in the second iteration of the loop
+          # fetch our prev_checkin object
+          if index > 0
+            # make a compound index
+            prev_checkin = Checkin.find_by_foursquare_id(checkins[index-1].id)
+            current_checkin = Checkin.find_by_foursquare_id(checkins[index].id)
+
+            # surely there's a nicer way to  write this?
+            l = user.legs.find_or_initialize_by_start_checkin_id_and_end_checkin_id(prev_checkin.id, current_checkin.id)
+
+            if l.new_record?
+              distance = Checkin.distance_between_points(current_checkin, prev_checkin).to_km
+
+              l.update_attributes!({
+               :distance => distance,
+               :co2 => Checkin.co2_for_km(distance),
+               :timestamp => current_checkin.timestamp,
+               :timezone => current_checkin.timezone,
+              })
+
+              l.save!
+            end
+
+          end
+        end
+      end
+      # TODO turn to named scope - return list of checkins from the last 7 days
+      @legs = user.legs.where("timestamp > ?", Date.current-107.day )
+
+      # using delay makes this act as a delayed job
+      # http://rdoc.info/github/collectiveidea/delayed_job/master/file/README.textile#Gory_Details
+      FootprintMailer.footprint_email(user, @legs, request.host_with_port).deliver!
+
+    end
+
+    class << self
+      handle_asynchronously :calculate_carbon_and_send_mail
+    end
+
+
+
+
+
 end
 
 class DistanceError < Exception ;end
